39a9928854d5294cee02f341ed2002b5d8a4b5dd
Sergio Duarte Correia <scorreia@ipportalegre.pt>
2023-07-18 15:58:44 +0100

    folders

---
+++ code/01_HardwareValidation/01_HardwareValidation/_01_HardwareValidation.ino
@@ -0,0 +1,20 @@
+/*
+ * _01_HardwareValidation.ino
+ *
+ * Created: 7/18/2023 1:20:00 PM
+ * Author: scorr
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
---
+++ code/01_HardwareValidation/01_HardwareValidation/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * 01_HardwareValidation.cpp
+ *
+ * Created: 18/07/2023 13:20:03
+ * Author : scorr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
